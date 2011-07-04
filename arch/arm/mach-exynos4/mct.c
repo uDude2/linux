da4e12e7f70b527b30fcaddbe08990007b3c52b7
MyungJoo Ham <myungjoo.ham@samsung.com>
2011-07-04 14:49:35 +0900

    ARM: EXYNOS4: Enable Suspend/Resume of MCT.
    Suspend/Resume has been failing because of MCT after introducing EXT-GIC
    to EXYNOS4. This patch allows MCT's PM to work properly.
    
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>

--- arch/arm/mach-exynos4/mct.c
+++ arch/arm/mach-exynos4/mct.c
@@ -26,6 +26,7 @@
 
 static unsigned long clk_cnt_per_tick;
 static unsigned long clk_rate;
+static cycle_t time_suspended;
 
 struct mct_clock_event_device {
 	struct clock_event_device *evt;
@@ -132,12 +133,26 @@ static cycle_t exynos4_frc_read(struct clocksource *cs)
 	return ((cycle_t)hi << 32) | lo;
 }
 
+static void exynos4_frc_suspend(struct clocksource *cs)
+{
+	time_suspended = exynos4_frc_read(cs);
+};
+
+static void exynos4_frc_resume(struct clocksource *cs)
+{
+	exynos4_mct_frc_start((u32)(time_suspended >> 32), (u32)time_suspended);
+
+	exynos4_mct_write(0x1, EXYNOS4_MCT_L0_BASE + MCT_L_TCNTB_OFFSET);
+};
+
 struct clocksource mct_frc = {
 	.name		= "mct-frc",
 	.rating		= 400,
 	.read		= exynos4_frc_read,
 	.mask		= CLOCKSOURCE_MASK(64),
 	.flags		= CLOCK_SOURCE_IS_CONTINUOUS,
+	.suspend	= exynos4_frc_suspend,
+	.resume		= exynos4_frc_resume,
 };
 
 static void __init exynos4_clocksource_init(void)
