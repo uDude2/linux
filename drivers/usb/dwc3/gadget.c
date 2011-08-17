3a9bd8204ca23b167367b09c1e17f864487ac3e7
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2011-08-17 18:24:27 +0200

    usb: dwc3: gadget: only stop a transfer if there is an active one
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1444,15 +1444,16 @@ static void dwc3_stop_active_transfer(struct dwc3 *dwc, u32 epnum)
 
 	dep = dwc->eps[epnum];
 
-	if (!(dep->flags & DWC3_EP_ENABLED))
-		return;
+	if (dep->res_trans_idx) {
+		cmd = DWC3_DEPCMD_ENDTRANSFER;
+		cmd |= DWC3_DEPCMD_HIPRI_FORCERM;
+		cmd |= DWC3_DEPCMD_PARAM(dep->res_trans_idx);
+		memset(&params, 0, sizeof(params));
+		ret = dwc3_send_gadget_ep_cmd(dwc, dep->number, cmd, &params);
+		WARN_ON_ONCE(ret);
 
-	cmd = DWC3_DEPCMD_ENDTRANSFER;
-	cmd |= DWC3_DEPCMD_HIPRI_FORCERM;
-	cmd |= DWC3_DEPCMD_PARAM(dep->res_trans_idx);
-	memset(&params, 0, sizeof(params));
-	ret = dwc3_send_gadget_ep_cmd(dwc, dep->number, cmd, &params);
-	WARN_ON_ONCE(ret);
+		dep->res_trans_idx = 0;
+	}
 
 	dwc3_gadget_nuke(dep, -ESHUTDOWN);
 }
@@ -1461,8 +1462,15 @@ static void dwc3_stop_active_transfers(struct dwc3 *dwc)
 {
 	u32 epnum;
 
-	for (epnum = 2; epnum < DWC3_ENDPOINTS_NUM; epnum++)
+	for (epnum = 2; epnum < DWC3_ENDPOINTS_NUM; epnum++) {
+		struct dwc3_ep *dep;
+
+		dep = dwc->eps[epnum];
+		if (!(dep->flags & DWC3_EP_ENABLED))
+			continue;
+
 		dwc3_stop_active_transfer(dwc, epnum);
+	}
 }
 
 static void dwc3_clear_stall_all_ep(struct dwc3 *dwc)
