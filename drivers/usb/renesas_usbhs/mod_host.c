ee5fcfe49a8b9f10dfa985af95f3eb7682ee2a56
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2011-10-23 19:56:30 -0700

    usb: gadget: renesas_usbhs: remove usbhs_sys_usb_ctrl()
    usbhs_sys_usb_ctrl() can collect into usbhs_sys_host/function_ctrl().
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/renesas_usbhs/mod_host.c
+++ drivers/usb/renesas_usbhs/mod_host.c
@@ -1200,7 +1200,6 @@ static int usbhsh_start(struct usbhs_priv *priv)
 	 * - usb module
 	 */
 	usbhs_sys_host_ctrl(priv, 1);
-	usbhs_sys_usb_ctrl(priv, 1);
 
 	/*
 	 * enable irq callback
@@ -1226,7 +1225,6 @@ static int usbhsh_stop(struct usbhs_priv *priv)
 
 	/* disable sys */
 	usbhs_sys_host_ctrl(priv, 0);
-	usbhs_sys_usb_ctrl(priv, 0);
 
 	dev_dbg(dev, "quit host\n");
 
